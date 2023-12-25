0c68a3134f0d672e685f2ba96c3991385ac18f02
yegao <familyink@163.com>
2023-12-25 20:22:50 +0800

    5.0.1

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 module.exports = async function(source) {
     const options = this.getOptions();
-    return source.replace(options.pattern, options.replacement);
+    if (options instanceof Array) {
+        for (let i = 0, len = options.length; i < len; i++) {
+            const one = options[i];
+            source = source.replace(one.pattern, one.replacement);
+        }
+    } else if (options instanceof Object) {
+        return source.replace(options.pattern, options.replacement);
+    }
+    return source;
 }
