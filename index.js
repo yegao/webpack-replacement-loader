4acb3b675e973bd3fd6fd61e64a09326ec68b49e
yegao <familyink@163.com>
2023-12-26 20:54:01 +0800

    1.0.2

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ module.exports = function(source) {
     if (options instanceof Array) {
         for (let i = 0, len = options.length; i < len; i++) {
             const one = options[i];
-            source = source.replace(one.pattern, one.replacement);
+            source = source.replaceAll(one.pattern, one.replacement);
         }
     } else if (options instanceof Object) {
-        return source.replace(options.pattern, options.replacement);
+        return source.replaceAll(options.pattern, options.replacement);
     }
     return source;
 }
